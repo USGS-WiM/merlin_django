e8f87ad447059ed7afcc7df731e0dbe26c806dfc
AaronStephenson <astephenson@usgs.gov>
2014-12-03 11:00:00 -0600

    Changed titles/headers in HTML to MeRLn from Mercury Lab. Added Bottle Range Add form to Bottles page. Fixed Reload buttons. Fixed bug in Edit and Delete buttons (they all pointed to Acids endpoints). Nav bar now open by default on Index page. Cleaned up the Nav list.

--- mercuryproject/urls.py
+++ mercuryproject/urls.py
@@ -2,11 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mercuryproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^mercuryadmin/', include(admin.site.urls)),
     url(r'^mercuryservices/', include('mercuryservices.urls')),# namespace="mercuryservices")),
+    url(r'^mercurydocs/', include('rest_framework_swagger.urls')),
     url(r'^mercurylab/', include('mercurylab.urls', namespace="mercurylab")),
 )
