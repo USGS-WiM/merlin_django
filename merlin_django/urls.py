f01935d75a62b772abce4037409c774010501814
Aaron Stephenson <astephenson@usgs.gov>
2021-12-08 15:34:28 -0600

    Re-enable the built-in admin app, for user management

--- merlin_django/urls.py
+++ merlin_django/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 
 
 urlpatterns = [
     url(r'^merlinservices/', include('merlinservices.urls')),
     url(r'^merlinauth/', include('djoser.urls')),
+    url(r'^merlinadmin/', admin.site.urls),
     url(r'^merlin/', include('merlin.urls')),
 ]
